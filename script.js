075a7e22dc30bc3a9bf3d87263b8902995b8b11c
K KIRAN <111232471+kamathamkiran@users.noreply.github.com>
2023-06-24 20:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
-let result = document.getElementById('result');
+var screen = document.querySelector('#screen');
 
-function insert(value) {
-  result.value += value;
+function insert(value) 
+{
+  screen.value += value;
+}
+function sin()
+{
+  screen.value = Math.sin(eval(screen.value));
+}
+function cos()
+{
+  screen.value = Math.cos(eval(screen.value));
+}
+function tan()
+{
+  screen.value = Math.tan(eval(screen.value));
+}
+function asin()
+{
+  screen.value = Math.asin(eval(screen.value));
+}
+function acos()
+{
+  screen.value = Math.acos(eval(screen.value));
+}
+function atan()
+{
+  screen.value = Math.atan(eval(screen.value));
+}
+function power()
+{
+  screen.value = Math.pow(eval(screen.value),2);
+}
+function sqrt()
+{
+  screen.value = Math.sqrt(eval(screen.value));
+}
+function log()
+{
+  screen.value = Math.log(eval(screen.value));
+}
+function factorial()
+{
+    var res = 1, num = eval(screen.value), i;
+    for(i = 1; i <= num; i++)
+    {
+       res = res*i;
+    }
+    screen.value = res;
+}
+function back()
+{
+    screen.value = screen.value.substr(0,screen.value.length-1);
+}
+function calculate()
+{
+  screen.value = eval(screen.value);
+}
+function clearAll()
+{
+  screen.value = ''; 
 }
 
-function clearAll() {
-  result.value = '';
-}
-
-function calculate() {
-  result.value = eval(result.value);
-}
+console.log(eval('2+3'));
